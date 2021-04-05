546bae21d50626807c5edf94a8c3eb481b8662a4
PratilipiAich <pratilipi@wallmag.io>
2021-04-05 14:30:30 +0530

    Responsive design

--- script.js
+++ script.js
@@ -29,153 +29,153 @@ let All_song = [
    {
      name: "Boombayah",
      path: "http://docs.google.com/uc?export=open&id=1le5oYRWyWafVBmNDIXyzLvXPT1cLEh9P",
-     img: "https://i.imgur.com/925rNef.png",
-     singer: "2016"
+     img: "./assets/boombayah.png",
+     singer: "08 August, 2016"
    },
    {
      name: "Whistle",
      path: "http://docs.google.com/uc?export=open&id=1jyaJi1uTF4Ues-LouSAxE9KoH8TIVuAG",
-     img: "https://i.vimeocdn.com/video/585679807_1280x720.jpg",
-     singer: "2016"
+     img: "./assets/whistle.jpg",
+     singer: "08 August, 2016"
    },
    {
      name: "Playing With Fire",
      path: "http://docs.google.com/uc?export=open&id=1k1NJvUCDCBfDPaZ1_VAWUvZOBKA3Jk2V",
-     img: "https://is4-ssl.mzstatic.com/image/thumb/Video118/v4/a1/e4/95/a1e495c4-76da-c99b-ad92-1a9d627ac9fb/KRZ351600028.jpg/1920x1080mv.jpg",
-     singer: "2016"
+     img: "./assets/playing.jpg",
+     singer: "01 November, 2016"
    },
    {
      name: "Stay",
      path: "http://docs.google.com/uc?export=open&id=1jiTwPgP2j4FUROyaEcDRlaOdUt-9vN3K",
-     img: "https://static.wikia.nocookie.net/blinks/images/1/16/BLACKPINK_-_%27STAY%27_M_V/revision/latest/top-crop/width/450/height/450?cb=20170218191827",
-     singer: "2016"
+     img: "./assets/stay.jpg",
+     singer: "01 November, 2016"
    },
    {
      name: "As if it's your last",
      path: "http://docs.google.com/uc?export=open&id=1nLXLPZGtJUrkdChYf0WJFH3CegSIbn0P",
-     img: "https://thebiaslistcom.files.wordpress.com/2017/06/blackpink-as-if-its-your-last.jpg",
-     singer: "2017"
+     img: "./assets/as.jpg",
+     singer: "22 June, 2017"
    },
+   {
+	name: "So Hot",
+	path: "http://docs.google.com/uc?export=open&id=1jkfd8o5Meaba4cUmHYffcP2rVSpLkMKy",
+	img: "./assets/hot.jpg",
+	singer: "25 December, 2017"
+  },
    {
 	name: "DU-DDU-DU-DDU",
 	path: "http://docs.google.com/uc?export=open&id=1hiJrAvKCLrA_cu2Hlz3oRDvS-O4dXl7w",
-	img: "https://portalpopline.com.br/wp-content/uploads/2020/12/blackpink-2.jpg",
-	singer: "2018"
+	img: "./assets/du.jpg",
+	singer: "15 June, 2018"
   },
   {
 	name: "Forever Young",
 	path: "http://docs.google.com/uc?export=open&id=1f0SFLw-ck0CQL9xigM3oipth3Do4OLSz",
-	img: "https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/styles/full/public/blackpink_3.jpg?itok=ghj3j9Ld&mtime=1529454531",
-	singer: "2018"
+	img: "./assets/forever.jpg",
+	singer: "15 June, 2018"
   },
   {
 	name: "Really Really",
 	path: "http://docs.google.com/uc?export=open&id=1joefzueBimsK3T0qrQpsBvNCD5Qnp-dj",
 	img: "https://i1.wp.com/blackpinkupdate.com/wp-content/uploads/2019/09/3-Photos-BLACKPINK-Summer-Diary-2019-in-Hawaii.jpg?fit=1080%2C720&ssl=1",
-	singer: "2018"
+	singer: "15 June, 2018"
   },
   {
 	name: "See You Later",
 	path: "http://docs.google.com/uc?export=open&id=1jn-p_z-hTuRxT3uK5GNy3ZupN_X4vaNq",
-	img: "https://0.soompi.io/wp-content/uploads/2019/08/27235036/blackpink-111.jpg",
-	singer: "2018"
+	img: "./assets/see.jpg",
+	singer: "15 June, 2018"
   },
   {
 	name: "Kiss and Makeup",
 	path: "http://docs.google.com/uc?export=open&id=1EqX2gyjypwcJlANe_QpRrsafJYGZUuEf",
-	img: "https://i.pinimg.com/originals/62/4d/27/624d27ecd36cd093773a1690d131e9f2.jpg",
-	singer: "Dua Lipa (2018)"
-  },
-  {
-	name: "So Hot",
-	path: "http://docs.google.com/uc?export=open&id=1jkfd8o5Meaba4cUmHYffcP2rVSpLkMKy",
-	img: "https://pbs.twimg.com/media/DSalaWTVQAAfvvJ.jpg",
-	singer: "2019"
+	img: "./assets/kiss.jpg",
+	singer: "Dua Lipa (19 October, 2018)"
   },
   {
 	name: "Solo",
 	path: "http://docs.google.com/uc?export=open&id=1_hOjVFz_ZzqtFQ8mdHdWwLxB1puUXwhc",
-	img: "https://malaysia-grlk5lagedl.stackpathdns.com/production/malaysia/images/1542093224773529-j17.jpg?w=1900&fit=crop&crop=faces&auto=%5B%22format%22%2C%20%22compress%22%5D&cs=srgb",
-	singer: "Jennie (2019)"
+	img: "./assets/solo.jpg",
+	singer: "Jennie (16 November, 2019)"
   },
  
   {
    name: "Kill This Love",
    path: "http://docs.google.com/uc?export=open&id=1jphJkCxfsx7wqWzSwZ9KIjGvUTnC69b_",
-   img: "https://static.billboard.com/files/media/blackpink-kill-this-love-MV-2019-billboard-1548-768x433.jpg",
-   singer: "2019"
+   img: "./assets/kill.jpg",
+   singer: "05 April, 2019"
  },
  {
    name: "Don't Know What To Do",
    path: "http://docs.google.com/uc?export=open&id=1lIKL8HkROOzbe0NuYP2YuwxV90KHwiRU",
-   img: "http://s8.picofile.com/file/8356707450/maxresdefault.jpg",
-   singer: "2019"
+   img: "./assets/dont.jpg",
+   singer: "05 April, 2019"
  },
  {
 	name: "Hope Not",
 	path: "http://docs.google.com/uc?export=open&id=1-7Sram2Mh7OhYCRAsdEcHnzq_ZnoiNkR",
-	img: "https://i0.wp.com/starmometer.com/wp-content/uploads/2019/02/04.jpg?w=1000&ssl=1",
-	singer: "2019"
+	img: "./assets/hope.jpg",
+	singer: "05 April, 2019"
   },
   {
 	name: "How You Like That",
 	path: "http://docs.google.com/uc?export=open&id=1-yav-MvmLq6jOS_zui3lWP5cY-sghIkm",
-	img: "https://ss-images.saostar.vn/w800/pc/1596897301918/blackpink-1(2).jpeg",
-	singer: "2019"
+	img: "./assets/how.jpeg",
+	singer: "26 June, 2019"
   },
   {
 	name: "Sour Candy",
 	path: "http://docs.google.com/uc?export=open&id=1yUYXUP0B-Mz8TfE_NOxPaAU-zp3yrBby",
 	img: "https://i.ytimg.com/vi/wgwMzdNneyI/maxresdefault.jpg",
-	singer: "Lady Gaga (2020)"
+	singer: "Lady Gaga (28 May, 2020)"
   },
   {
 	name: "Ice Cream",
 	path: "http://docs.google.com/uc?export=open&id=1sR_Ui74voYsHA1UfanQ67IcW0ZREsdWq",
-	img: "https://sm.mashable.com/t/mashable_in/photo/default/ice-cream-blackpink-and-selena-gomez-whip-up-the-only-treat_r8zd.h960.jpg",
-	singer: "Selena Gomez (2020)"
+	img: "./assets/sour.jpg",
+	singer: "Selena Gomez (28 August, 2020)"
   },
   {
 	name: "Lovesick Girls",
 	path: "http://docs.google.com/uc?export=open&id=1seOH82-Zeki3y89shw1TV9S7TsriUWLZ",
-	img: "https://imgix.bustle.com/uploads/image/2020/10/2/52312c03-29ea-4655-80c7-540b828e568d-blackpink-the-album-lovesick-girls-video.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format%2Ccompress",
-	singer: "2020"
+	img: "./assets/lovesick.jpg",
+	singer: "02 October, 2020"
   },
   {
 	name: "Pretty Savage",
 	path: "http://docs.google.com/uc?export=open&id=1sUX7MA-Jar6NBe6scY4CyApRufDOWS97",
-	img: "https://cdn0-production-images-kly.akamaized.net/EsbzQY8-jJnNInMHkr93_p5g45o=/673x379/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3265127/original/065247200_1602493244-bp2.jpg",
-	singer: "2020"
+	img: "./assets/pretty.jpg",
+	singer: "02 October, 2020"
   },
   {
 	name: "Crazy Over You",
 	path: "http://docs.google.com/uc?export=open&id=1rn6JHXJnrTfwu7Ynz2ZLX6S3wPYkugk_",
-	img: "https://thehoneypop.com/wp-content/uploads/2020/10/Sans-titre-19.png",
-	singer: "2020"
+	img: "./assets/crazy.png",
+	singer: "02 October, 2020"
   },
   {
 	name: "Love To Hate Me",
 	path: "http://docs.google.com/uc?export=open&id=1sCCGOYiojbjPZn5evMeYjimd45nj2Y_A",
-	img: "https://imgix.bustle.com/uploads/image/2021/2/1/a6a99936-ac8d-45af-ab8c-70b8361af462-the-show-12.jpg?w=1200&h=630&fit=crop&crop=faces&fm=jpg",
-	singer: "2020"
+	img: "./assets/love.jpg",
+	singer: "02 October, 2020"
   },
   {
 	name: "Bet You Wanna",
 	path: "http://docs.google.com/uc?export=open&id=1s0mOEsomI2nS5uw1VYwvqCNIlOfUCA92",
-	img: "https://thatgrapejuice.net/wp-content/uploads/2020/10/cardi-b-thatgrapejuice-blackpink-bet-wanna-1280x720.jpeg",
-	singer: "Cardi B (2020)"
+	img: "./assets/bet.jpeg",
+	singer: "Cardi B (02 October, 2020)"
   },
   {
 	name: "Gone",
 	path: "http://docs.google.com/uc?export=open&id=1-6DqkR01S4AnpTBD19W_fwE8nhPVHnvt",
-	img: "https://billboardvn.vn/wp-content/uploads/2021/01/rose-1.jpg",
-	singer: "Ros&eacute;  (2021)"
+	img: "./assets/gone.jpg",
+	singer: "Ros&eacute;  (04 April, 2021)"
   },
   {
 	name: "On The Ground",
 	path: "http://docs.google.com/uc?export=open&id=1-1Ak4-WAg8l7_PPhmrMKaElPe3C1D77D",
-	img: "https://www.vagalume.com.br/dynimage/news44104-big.jpg",
-	singer: "Ros&eacute;  (2021)"
+	img: "./assets/ground.jpg",
+	singer: "Ros&eacute;  (12 March, 2021)"
   },
 
  
